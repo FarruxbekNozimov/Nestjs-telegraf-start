9760e9087cf576df9d9b4fd0e37b53cfe30105d0
FarruxDEV <farruxbeknozimov@gmail.com>
2023-07-23 17:35:02 +0500

    enable to cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: ['*'],
+    credentials: true,
+  });
+
   await app.listen(7000, () => {
     console.log('server ishga tushdi');
   });
